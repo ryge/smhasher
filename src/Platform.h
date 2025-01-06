4dfeb94052b2f98350a3d8616ebae1b96071c324
andreas.ryge <andreas.ryge@choreograph.com>
2025-01-06 13:53:16 +0100

    Added identifcation of platform and adaptions to ARM64

--- src/Platform.h
+++ src/Platform.h
@@ -80,6 +80,10 @@ __inline__ unsigned long long int rdtsc()
     unsigned long long int x;
     __asm__ volatile ("rdtsc" : "=A" (x));
     return x;
+#elif defined(__aarch64__) 
+    unsigned long long int x;
+    __asm__ volatile("mrs %0, cntvct_el0" : "=r" (x));
+    return x;
 #else
 #define NO_CYCLE_COUNTER
     return 0;
@@ -92,3 +96,51 @@ __inline__ unsigned long long int rdtsc()
 #endif	//	!defined(_MSC_VER)
 
 //-----------------------------------------------------------------------------
+
+extern "C" {
+    inline const char *getArch() { //Get current architecture, detectx nearly every architecture. Coded by Freak
+        #if defined(__x86_64__) || defined(_M_X64)
+        return "x86_64";
+        #elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+        return "x86_32";
+        #elif defined(__ARM_ARCH_2__)
+        return "ARMv2";
+        #elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
+        return "ARMv3";
+        #elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
+        return "ARMv4-T";
+        #elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
+        return "ARMv5"
+        #elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
+        return "ARMv6-T2";
+        #elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
+        return "ARMv6";
+        #elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+        return "ARMv7";
+        #elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+        return "ARMv7-A";
+        #elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+        return "ARMv7-R";
+        #elif defined(__ARM_ARCH_7M__)
+        return "ARMv7-M";
+        #elif defined(__ARM_ARCH_7S__)
+        return "ARMv7-S";
+        #elif defined(__aarch64__) || defined(_M_ARM64)
+        return "ARM64/AArch64";
+        #elif defined(mips) || defined(__mips__) || defined(__mips)
+        return "MIPS";
+        #elif defined(__sh__)
+        return "SUPERH";
+        #elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+        return "POWERPC";
+        #elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
+        return "POWERPC64";
+        #elif defined(__sparc__) || defined(__sparc)
+        return "SPARC";
+        #elif defined(__m68k__)
+        return "M68K";
+        #else
+        return "UNKNOWN";
+        #endif
+    }
+}
